1b420c88b936df9e5a0eb8a1f54401776c64bce7
wjr21 <wjr21@sussex.ac.uk>
2022-07-27 16:25:17 +0100

    Sampling every 10th data point

--- color_z_confusion.py
+++ color_z_confusion.py
@@ -53,8 +53,8 @@ for ax, (c1, c2) in zip(axes, cols):
     print(c1, "-", c2)
 
     # Get fluxes
-    f1 = hdf[c1][::4]
-    f2 = hdf[c2][::4]
+    f1 = hdf[c1][::10]
+    f2 = hdf[c2][::10]
 
     # Get color
     col = 2.5 * np.log10(f1 / f2)
